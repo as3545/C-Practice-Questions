c9709e78cbd09c995da5a88258804de236f8e86b
as3545 <86479682+as3545@users.noreply.github.com>
2024-09-15 11:16:42 +0530

    Remove all character except alphabet.cpp

---
+++ Remove all character except alphabet.cpp
@@ -0,0 +1,16 @@
+#include<cctype>
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+string s;
+cin>>s;
+string result;  
+for(char c:s){
+if(isalpha(c)){
+result +=c;
+}
+}
+cout<<result<<endl;
+return 0;
+}
